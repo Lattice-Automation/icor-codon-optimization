6f8b95f6edb687dd1e441c6941a9392208c492f6
RJain12 <397630@bsd48.org>
2021-06-30 06:33:04 +0000

    naive seqs

--- scripts/naive_optimizer.py
+++ scripts/naive_optimizer.py
@@ -1,54 +1,60 @@
-# Naively optimizes a directory containing amino acid sequences in the FASTA format and saves these "optimized" / "generated" DNA sequences in a directory.
-
-import random
 import os
 from Bio import SeqIO
-import re
 from Bio.Seq import Seq
+import random
+import numpy as np
+import re
+
+#frequencies are from ecoli_codon_frequencies.xlsx file in summaries dir
+#create dict with value being a tuple with the codons and their probabilities/frequncies
+frequency = {
+    "A": (["GCG", "GCA", "GCT", "GCC"],[0.34, 0.22, 0.17, 0.27]),
+    "R": (["AGG", "AGA", "CGG","CGA","CGT","CGC"],[0.03, 0.05, 0.1, 0.07, 0.37, 0.38]),
+    "N": (["AAT", "AAC"],[0.46, 0.54]),
+    "D": (["GAT","GAC"],[0.63,0.37]),
+    "C": (["TGT","TGC"],[0.45,0.55]),
+    "*": (["TGA","TAG","TAA"],[0.3,0.08,0.62]),
+    "Q": (["CAG","CAA"],[0.66,0.34]),
+    "E": (["GAG","GAA"],[0.32,0.68]),
+    "G": (["GGG","GGA","GGT","GGC"],[0.15,0.12,0.34,0.39]),
+    "H": (["CAT","CAC"],[0.57,0.43]),
+    "I": (["ATA","ATT","ATC"],[0.09,0.5,0.41]),
+    "L": (["TTG","TTA","CTG","CTA","CTT","CTC"],[0.13,0.13,0.49,0.04,0.11,0.1]),
+    "K": (["AAG","AAG"],[0.25,0.75]),
+    "M": (["ATG"],[1.0]),
+    "F": (["TTT","TTC"],[0.57,0.43]),
+    "P": (["CCG","CCA","CCT","CCC"],[0.51,0.2,0.17,0.12]),
+    "S": (["AGT","AGC","TCG","TCA","TCT","TCC"],[0.15,0.26,0.15,0.13,0.16,0.15]),
+    "T": (["ACG","ACA","ACT","ACC"],[0.26,0.15,0.18,0.42]),
+    "W": (["TGG"],[1.0]),
+    "Y": (["TAT","TAC"],[0.58,0.42]),
+    "V": (["GTG","GTA","GTT","GTC"],[0.36,0.16,0.27,0.21])
+}
 
 # Amino acid sequence dir to optimize:
-aa_dir = r"C:\Users\risha\Desktop\icor-codon-optimization\benchmark_sequences\aa"
+aa_dir = r"/workspace/icor-codon-optimization/benchmark_sequences/aa"
 
 # Output dir to store optimized seqs:
-out_dir = r"C:\Users\risha\Desktop\icor-codon-optimization\benchmark_sequences\naive"
+out_dir = r"/workspace/icor-codon-optimization/benchmark_sequences/naive"
 
-# Amino acid to codon table, outputs arr of codons:
-def aa2codons(seq : str) -> list:
-    _aas = {
-        "A": ["GCT GCC GCA GCG"],
-        "R": ["CGT CGC CGA CGG AGA AGG"],
-        "N": ["AAT AAC"],
-        "D": ["GAT GAC"],
-        "C": ["TGT TGC"],
-        "Q": ["CAA CAG"],
-        "E": ["GAA GAG"],
-        "G": ["GGT GGC GGA GGG"],
-        "H": ["CAT CAC"],
-        "I": ["ATT ATC ATA"],
-        "L": ["TTA TTG CTT CTC CTA CTG"],
-        "K": ["AAA AAG"],
-        "M": ["ATG ATG"],
-        "F": ["TTT TTC"],
-        "P": ["CCT CCC CCA CCG"],
-        "S": ["TCT TCC TCA TCG AGT AGC"],
-        "T": ["ACT ACC ACA ACG"],
-        "W": ["TGG TGG"],
-        "Y": ["TAT TAC"],
-        "V": ["GTT GTC GTA GTG"],
-        "B": ["GAT GAC AAT AAC"],
-        "Z": ["GAA GAG CAA CAG"],
-        "*": ["TAA TAG TGA"],
-    }
-    return [_aas[i] for i in seq]
+
+# Normalize probabilities for frequency if sum is not exactly 1.
+def fix_p( p):
+    if p.sum() != 1.0:
+        p = p*(1./p.sum())
+    return p
 
 for entry in os.scandir(aa_dir):
     name = entry.name[0:-9] + "_dna"
-    record = SeqIO.read(entry,'fasta')
-    arr = []
 
-    for i in record.seq:
-        arr.append(random.choice(aa2codons(i)[0][0].split()))
-
-    record.seq = Seq(re.sub('[^GATC]',"",str("".join(arr)).upper()))
+    # Replace ambiguities with amino acids from IUPAC guidelines.
+    record = SeqIO.read(entry,"fasta")
+    seq = record.seq.replace("B", random.choice(["D","N"])).replace("Z", random.choice(["E", "Q"]))
+    seq_arr = []
+    for aa in seq:
+        #append to the array a random choice of codon using the probabilities given (p)
+        seq_arr.append(np.random.choice(frequency[aa][0],p=fix_p(np.asarray(frequency[aa][1]))))
+    
+    record.seq = Seq(re.sub('[^GATC]',"",str("".join(seq_arr)).upper()))
     complete_name = os.path.join(out_dir, name)
     SeqIO.write(record, complete_name, "fasta")
