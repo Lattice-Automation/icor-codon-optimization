ed7d8632d5f39575dd87bd546025ca3385de974d
RJain12 <397630@bsd48.org>
2022-10-20 11:08:31 -0700

    HFC update & Fixes

--- tool/optimizers/HFC_optimizer.py
+++ tool/optimizers/HFC_optimizer.py
@@ -9,27 +9,27 @@ import re
 # frequencies are from ecoli_codon_frequencies.xlsx file in summaries dir
 # create dict with value being a tuple with the codons and their probabilities/frequncies
 frequency = {
-    "A": (["GCG", "GCA", "GCT", "GCC"], [0.34, 0.22, 0.17, 0.27]),
-    "R": (["AGG", "AGA", "CGG", "CGA", "CGT", "CGC"], [0.03, 0.05, 0.1, 0.07, 0.37, 0.38]),
-    "N": (["AAT", "AAC"], [0.46, 0.54]),
-    "D": (["GAT", "GAC"], [0.63, 0.37]),
-    "C": (["TGT", "TGC"], [0.45, 0.55]),
-    "*": (["TGA", "TAG", "TAA"], [0.3, 0.08, 0.62]),
-    "Q": (["CAG", "CAA"], [0.66, 0.34]),
-    "E": (["GAG", "GAA"], [0.32, 0.68]),
-    "G": (["GGG", "GGA", "GGT", "GGC"], [0.15, 0.12, 0.34, 0.39]),
-    "H": (["CAT", "CAC"], [0.57, 0.43]),
-    "I": (["ATA", "ATT", "ATC"], [0.09, 0.5, 0.41]),
-    "L": (["TTG", "TTA", "CTG", "CTA", "CTT", "CTC"], [0.13, 0.13, 0.49, 0.04, 0.11, 0.1]),
-    "K": (["AAG", "AAG"], [0.25, 0.75]),
-    "M": (["ATG"], [1.0]),
-    "F": (["TTT", "TTC"], [0.57, 0.43]),
-    "P": (["CCG", "CCA", "CCT", "CCC"], [0.51, 0.2, 0.17, 0.12]),
-    "S": (["AGT", "AGC", "TCG", "TCA", "TCT", "TCC"], [0.15, 0.26, 0.15, 0.13, 0.16, 0.15]),
-    "T": (["ACG", "ACA", "ACT", "ACC"], [0.26, 0.15, 0.18, 0.42]),
-    "W": (["TGG"], [1.0]),
-    "Y": (["TAT", "TAC"], [0.58, 0.42]),
-    "V": (["GTG", "GTA", "GTT", "GTC"], [0.36, 0.16, 0.27, 0.21])
+    "A": "GCG",
+    "R": "CGT",
+    "N": "AAC",
+    "D": "GAT",
+    "C": "TGC",
+    "*": "TAA",
+    "Q": "CAG",
+    "E": "GAA",
+    "G": "GGC",
+    "H": "CAT",
+    "I": "ATT",
+    "L": "CTG",
+    "K": "AAA",
+    "M": "ATG",
+    "F": "TTT",
+    "P": "CCG",
+    "S": "AGC",
+    "T": "ACC",
+    "W": "TGG",
+    "Y": "TAT",
+    "V": "GTG"
 }
 
 # Amino acid sequence dir to optimize:
@@ -50,7 +50,7 @@ for entry in os.scandir(aa_dir):
     seq_arr = []
     for aa in seq:
         # append to the array a random choice of codon using the probabilities given (p)
-        seq_arr.append(frequency[aa][0][np.argmax(frequency[aa][1])])
+        seq_arr.append(frequency[aa])
 
     record.seq = Seq(re.sub('[^GATC]', "", str("".join(seq_arr)).upper()))
     complete_name = os.path.join(out_dir, name)
